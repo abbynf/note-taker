0abd7a9f6b93e015f977ee7d25fc5364a2407668
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-07 20:52:00 -0600

    change / route to *

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 

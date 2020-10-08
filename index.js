b3bdc9723d94e9eb992c2555af457ec356e0ab70
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-07 21:05:33 -0600

    added route to send json of notes

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var notes = {
+    "John": "doe",
+    "Harry": "Potter"
+}
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
+
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
+var notesData = require("./db/db.json");
+
+app.get("/api/notes", function(req, res) {
+    return res.json(notesData);
+  });
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})

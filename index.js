2c0ade6fafbfd91eb6b85ed37fa8d74b26bb1480
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-07 20:50:23 -0600

    added route to display notes.html

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
 
-  app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
-  })
-  
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+

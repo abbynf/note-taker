657f2980d0a364629663c48d6abe89a69a58b08f
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-10-09 20:07:49 -0600

    added route for post api notes

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var db = require("./db/db.json");
+
+var fs = require("fs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use( express.static(__dirname + '/public' ) );
 
 app.get("/notes", function (req, res) {
@@ -11,8 +18,6 @@ app.get("/notes", function (req, res) {
 })
 
 var notesData = require("./db/db.json");
-
-console.log(notesData.length)
 
 app.get("/api/notes", function(req, res) {
     return res.json(notesData);
@@ -22,6 +27,32 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
+function newId(note){
+  return note.title;
+}
+
+app.post("/api/notes", function(req, res) {
+  var newSavedNote = {
+    "title" : req.body.title,
+    "text" : req.body.text,
+    "id" : newId(req.body)
+  }
+  var strNote = JSON.stringify(newSavedNote)
+  fs.readFile('./db/db.json', 'utf8', function (err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    obj.push(newSavedNote); //add some data
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile('./db/db.json', json, 'utf8', function (err){
+      if (err) {
+        console.log(err)
+      }
+    }); // write it back 
+}});
+  res.send(true);
+})
 
 //   Keep this after all other routes
   app.get("*", function (req, res) {
